ba12c6b49af5e74912b4f704e67c070969c0fa0b
Dekita <dekitarpg@gmail.com>
2021-12-22 08:18:07 +0000

    initial commit
    Doing an initial commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# EmbedMD
- Simple module to convert md files into 
+# README #
+EmbedMD written by dekitarpg@gmail.com
+
+This simple module allows you to load a markdown file, or directory of, and then convert the file(s) into discord.js embed objects!
+
+### Require EmbedMD ###
+```js
+// EmbedMD specific code:
+const EmbedMD = require('embed-md');// load module
+```
+
+### EmbedMD File Structure ###
+```md
+# TITLE #
+content 
+
+# COLOR #
+default
+
+# AUTHOR #
+user.name, user.avatar, https://discord.com/
+
+# THUMBNAIL #
+bot.avatar
+
+# DESCRIPTION #
+Some description...
+
+# FOOTER #
+user.name, user.avatar
+
+# FIELD #
+label, string, true
+
+# FIELDS #
+label, string, true
+
+# TIMESTAMP #
+false
+```
+
+### Load single file and get embed ###
+```js
+const embed_md = EmbedMD.prepareMD('/directory/path'); // cache embed files from path
+const embed = EmbedMD.getEmbed(embed_md); // get embed using embed_md
+```
+
+### Load all files from directory and get embed ###
+```js
+const embed_mds = EmbedMD.parseDir('/directory/path'); // cache embed files from path
+const embed_ids = Object.keys(embed_mds); // array of filenames without.md and route
+const embed = EmbedMD.getEmbed(embed_mds.embed_id); // get embed using embed_id
+```
+
+### PRO GAMER ###
+Its entirely possible to have the embed parse information based on additional properties. For example, you might want to display user information or some data from a variable within your embed. Simply pass an object as the second argument to `getEmbed` with the properties that you want to replace within your .md file. For example, lets assume the following md file:
+```md
+# DESCRIPTION #
+This is an example for how to use dynamic data with EmbedMD! The data shown within the footer is dynamic based on the properties given during the `getEmbed` function call
+
+# FOOTER #
+user.name, user.avatar
+``` 
+
+```js
+const embed = EmbedMD.getEmbed(embed_mds.embed_id, {
+    'user.avatar': interaction.user.displayAvatarURL(),
+    'user.name': interaction.user.username,
+}); 
+```
+
+### Send embed with discord.js ###
+```js
+// in some discord js command interaction:
+await interaction.reply({embeds: [embed]});
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,139 @@
+
+const { readdirSync, readFileSync } = require('fs');
+const {MessageEmbed} = require('discord.js');
+const embed_cache = {};
+
+class EmbedMD {
+
+    // helper function to format strings using objects
+    // ie: format("Hi name!", {name: 'DekiaRPG});
+    // => "Hi DekitaRPG!"
+    static format(str, objekt) {
+        const regstr = Object.keys(objekt).join("|");
+        const regexp = new RegExp(regstr,"gi");
+        return str.replace(regexp, matched => {
+            return objekt[matched.toLowerCase()];
+        });
+    }    
+
+    static scanDir(directory) {
+        return readdirSync(directory).filter(f => f.endsWith('.md'));
+    }
+
+    static parseDir(directory) {
+        const returned_data = {};
+        const info_files = this.scanDir(directory);
+        for (const file of info_files) {
+            const file_id = file.replace('.md','');
+            const filename = `${directory}/${file}`;
+            const file_data = this.prepareMD(filename);
+            returned_data[file_id] = file_data;
+        }
+        return returned_data;
+    }
+
+    static prepareMD(filename, refresh_cache=false) {
+        if (!embed_cache[filename] || refresh_cache) {
+            embed_cache[filename] = {
+                raw: readFileSync(filename, 'utf8'),
+                description: undefined,
+                thumbnail: undefined,
+                timestamp: undefined,
+                title: undefined,
+                color: undefined,
+                author: [],
+                footer: [],
+                fields: [],
+                filename,
+            };
+        }
+        return embed_cache[filename]; 
+    }
+
+    static parseMD(filename, replacers={}, refresh_cache=false) {
+        const embed_data = this.prepareMD(filename, refresh_cache);
+        const chunks = this.format(embed_data.raw, replacers).split('#');
+        chunks.shift() // <- discard before the first #
+        while (chunks.length) {
+            const type = (chunks.shift()||"").trim().toLowerCase();
+            const data = (chunks.shift()||"").trim();
+            switch (type.toUpperCase()) {
+                case 'URL':
+                case 'TITLE': 
+                case 'COLOR': 
+                case 'IMAGE': 
+                case 'THUMBNAIL': 
+                case 'TIMESTAMP': 
+                case 'DESCRIPTION': 
+                embed_data[type] = data.trim(); 
+                break;
+    
+                case 'AUTHOR': 
+                case 'FOOTER': 
+                embed_data[type] = data.split(',').map(d=>d.trim());
+                break;
+    
+                case 'FIELDS': 
+                const field_lines = data.split(/\r\n|\n\r|\n|\r/);
+                for (const field of field_lines) {
+                    const [name, value, inline] = this.parseDataArray(field.split(','));
+                    embed_data.fields.push({ name, value, inline })
+                }
+                break;
+    
+                case 'FIELD': 
+                const [name, value, inline] = this.parseDataArray(data.split(','));
+                embed_data.fields.push({ name, value, inline })
+                break;
+            }
+        }
+
+        const { raw, ...to_log} = embed_data;
+        // console.log('embed_data:', to_log);
+        return embed_data;
+    }
+
+    // convert arrays like ['1', '5', 'false', 'some description']
+    // to arrays like [1, 5, false, 'some description']
+    static parseDataArray(field_data_array) {
+        return field_data_array.map(element => {
+            const v = element.toLowerCase().trim();
+            const is_bool = ['true','false'].includes(v); 
+            const value = is_bool ? v === 'true' : (isNaN(v) ? v.trim() : parseInt(v));
+            return value;
+        });
+    }
+
+    static getEmbed(md_embed_object, ...other) {
+        const embed = new MessageEmbed();
+        const embed_data = this.parseMD(md_embed_object.filename, ...other);
+        for (const embed_element_data in embed_data) {
+            if (!Object.hasOwnProperty.call(embed_data, embed_element_data)) continue;
+            const element_funk = this.function_map[embed_element_data];
+            const element_data = embed_data[embed_element_data];
+            if (!embed[element_funk]) continue;
+            if (Array.isArray(element_data)) {
+                embed[element_funk](...element_data);
+            } else {
+                embed[element_funk](element_data);
+            }
+        }
+        return embed;
+    }
+}
+
+EmbedMD.function_map = {
+    url: 'setURL',
+    title: 'setTitle',
+    color: 'setColor',
+    author: 'setAuthor',
+    description: 'setDescription',
+    thumbnail: 'setThumbnail',
+    fields: 'addFields',
+    image: 'setImage',
+    timestamp: 'setTimestamp',
+    footer: 'setFooter',
+}
+Object.freeze(EmbedMD.function_map);
+
+module.exports = EmbedMD;
